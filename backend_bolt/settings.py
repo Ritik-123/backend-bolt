800f4d2143494460196c677009e8121fb42162d7
ritik-s-rapifuzz <ritiks@rapifuzz.com>
2025-06-17 08:38:22 +0530

    Modify docker-compose when we use websockets

--- backend_bolt/settings.py
+++ backend_bolt/settings.py
@@ -205,6 +205,7 @@ CHANNEL_LAYERS= {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
+            # "hosts": [("redis", 6379)], # For docker 
         }
     }
 }
